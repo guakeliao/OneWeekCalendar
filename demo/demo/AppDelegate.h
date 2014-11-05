f505df1b582716699374bd6249e41e3b7b2e8776
liaowei <598951856@qq.com>
2014-11-05 15:21:34 +0800

    no message

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by guakeliao on 14/11/5.
+//  Copyright (c) 2014年 Boco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by guakeliao on 14/11/5.
+//  Copyright (c) 2014年 Boco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
